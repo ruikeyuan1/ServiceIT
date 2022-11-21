f7dadcce32b93b8992b281f58d59795ed9ca75e7
ruikeyuan1 <keketty@163.com>
2022-11-21 18:46:50 +0100

    add pages folder to rick's branch for html/php pages

---
+++ pages/adminPanel.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<form action="firstCommitTest.php" method="post">
+    <!--        <label for="fname">First name:</label><br>-->
+    <!--        <input type="text" id="fname" name="fname" value="John"><br>-->
+    <!--        <label for="lname">Last name:</label><br>-->
+    <!--        <input type="text" id="lname" name="lname" value="Doe"><br><br>-->
+    <input type="submit" name="littlePost" value="Submit">
+</form>
+</body>
+</html>
+
+<?php
+
+    
+
+
+?>
