4b7e2e4e92e5de8478c1f0ba5f42bdbf831de162
ruikeyuan1 <keketty@163.com>
2022-11-27 15:23:30 +0100

    moved stylesheet

--- pages/adminPanel.php
+++ pages/adminPanel.php
@@ -159,7 +159,7 @@ function updateService($serviceNameSelected, $serviceStatusSelected, $serviceIdS
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel = "stylesheet" href="style/stylesheet.css" type="text/css">
+    <link rel = "stylesheet" href="stylesheet.css" type="text/css">
     <title>Admin Panel</title>
 </head>
 <body class="adminPanelPage">
