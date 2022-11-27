e3b6f363aef18ed3787ba544566380a5aaa1d1b5
Polinalavender <91316935+Polinalavender@users.noreply.github.com>
2022-11-27 16:09:55 +0100

    button change

--- pages/logInPage.php
+++ pages/logInPage.php
@@ -10,13 +10,13 @@ include "connect.php";
     <meta charset = "UTF-8">
     <meta http-equiv = "X-UA-Compatible" content="IE=edge">
     <meta name = "viewport" content="width=device-width, initial-scale=1.0">
-    <link rel = "stylesheet" href="Stylesheet.css" type="text/css">
+    <link rel = "stylesheet" href="stylesheet.css" type="text/css">
     <title>Service IT</title>
 </head>
 <body>
     <div class = "header">
         <div class = "logo">Service IT</div>
-        <button1 name = "admin">admin</button1>
+        <button1 name = "admin" onclick="document.location='adminLog.php'">admin</button1>
     </div>
     <div class = "container" id = "container"> 
         <h1>Welcome to Service IT</h1>
