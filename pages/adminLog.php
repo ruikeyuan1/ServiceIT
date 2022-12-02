d514ddbf2315d4d641d5eec085f727eb83fa549f
Polinalavender <91316935+Polinalavender@users.noreply.github.com>
2022-12-02 22:35:30 +0100

    Log in for admin

--- pages/adminLog.php
+++ pages/adminLog.php
@@ -1,30 +1,109 @@
-<!-- <?php
+<?php
 session_start();
-include "connect.php";
-include_once "utils.php";
-?> -->
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: adminPanel.php");
+    exit;
+}
+
+require_once "connect.php";
+
+$username = $password = "";
+$username_err = $password_err = $login_err = "";
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    // Check if username is empty
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter username.";
+    } else{
+        $username = trim($_POST["username"]);
+    }
+    
+    // Check if password is empty
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter your password.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    // Validate credentials
+    if(empty($username_err) && empty($password_err)){
+        
+        $sql = "SELECT `id`, `username`, `password` FROM administrator WHERE username = ?";
+        
+        if($stmt = $conn->prepare($sql)){
+            
+            $stmt->bind_param("s", $param_username);
+
+            $param_username = $username;
+
+            if($stmt->execute()){
+
+                $stmt->store_result();
+                
+                // Check if username exists, if yes then verify password
+                if($stmt->num_rows == 1){                    
+                        
+                    $stmt->bind_result($id, $username, $hashed_password);
+                    if($stmt->fetch()){
+                        if(password_verify($password, $hashed_password)){
+                            // Password is correct, so start a new session
+                            session_start();
+                            
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["username"] = $username;                            
+                            
+                            header("location: adminPanel.php");
+                        } else{
+                            $login_err = "Invalid username or password.";
+                        }
+                    }
+                } else{
+                    $login_err = "Invalid username or password.";
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            $stmt->close();
+        }
+    }
+    
+    $conn->close();
+}
+?>
+
 <!DOCTYPE html>
 <html lang = "en">
 <head>
     <meta charset = "UTF-8">
     <meta http-equiv = "X-UA-Compatible" content="IE=edge">
     <meta name = "viewport" content="width=device-width, initial-scale=1.0">
-    <link rel = "stylesheet" href="Stylesheet.css" type="text/css">
+    <link rel = "stylesheet" href="stylesheet.css" type="text/css">
     <title>Service IT</title>
 </head>
 <body>
     <div class = "header">
         <div class = "logo">Service IT</div>
-        <button1 name = "client" onclick="document.location='logInPage.php'">client</button1>
-
+        <button1 name = "client" onclick="document.location='LogInPage.php'">client</button1>
     </div>
     <div class = "container" id = "container"> 
         <h1>Welcome to Service IT</h1>
         <h2>Log in</h2>
+            <?php 
+            if(!empty($login_err)){
+                echo '<div class="alert alert-danger">' . $login_err . '</div>';
+            }        
+            ?>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <input type = "text" name = "username" placeholder = "Enter a username" <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+            <span><?php echo $username_err; ?></span>
+            
+            <input type="password" name="password" placeholder = "Enter a password" <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
+            <span><?php echo $password_err; ?></span>
 
-        <form> 
-            <input type = "text" name = "Username" placeholder = "Enter a username"/>
-            <input type = "password" name = "Password" placeholder = "Enter a password"/>
             <p>Forgot your password?</p>
             <button name = "Sign_In">Sign In</button>
         </form>
