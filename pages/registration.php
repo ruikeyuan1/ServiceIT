e5c078c5f975ced720c566958bef5b5fed08358a
Polinalavender <91316935+Polinalavender@users.noreply.github.com>
2022-12-05 22:52:04 +0100

    email check + added name field

--- pages/registration.php
+++ pages/registration.php
@@ -1,8 +1,8 @@
 <?php
 require_once "connect.php";
 
-$username = $password = $email = $confirm_password = "";
-$username_err = $password_err = $confirm_password_err = $email_err = "";
+$username = $password = $email = $confirm_password = $name = "";
+$username_err = $password_err = $confirm_password_err = $email_err = $name_err = "";
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 
@@ -38,6 +38,23 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         }
     }
     
+    //Validate name 
+    if(empty(trim($_POST["userName"]))){
+        $name_err = "Please enter a name.";
+    } elseif(!preg_match('/^[a-zA-Z]+$/', trim($_POST["userName"]))){
+        $name_err = "Username can only contain letters";
+    } else{
+        $name = trim($_POST["userName"]);
+    }
+
+    //Validate email
+    if(empty(trim($_POST["email"]))){
+        $email_err = "Please enter a password.";   
+    } elseif(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $password_err = "Please correct email";
+    } else{
+        $email = trim($_POST["email"]);
+    }
 
     // Validate password
     if(empty(trim($_POST["password"]))){
@@ -57,28 +74,21 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             $confirm_password_err = "Password did not match.";
         }
     }
-    
-    //Validate email
-    if(filter_var("some@address.com", FILTER_VALIDATE_EMAIL)) {
-        $email_err = "Please enter an email."; 
-    }
-    else {
-        $email_err = "Invaid email.";
-        $email = trim($_POST["email"]);
-    }
 
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err) && empty($email_err)){
+    
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err) && empty($email_err) && empty($name_err)){
         
-        $sql = "INSERT INTO user (`username`, `password`, `email`) VALUES (?, ?, ?)";
+        $sql = "INSERT INTO user (`username`, `password`, `email`, `name`) VALUES (?, ?, ?, ?)";
 
         if($stmt = $conn->prepare($sql)){
 
-            $stmt->bind_param("sss", $param_username, $param_password, $param_email);
+            $stmt->bind_param("ssss", $param_username, $param_password, $param_email, $param_name);
             
             $param_username = $username;
              // password hash
             $param_password = password_hash($password, PASSWORD_DEFAULT);
             $param_email = $email;
+            $param_name = $name;
 
             if($stmt->execute()){
                 header("location: logInPage.php");
@@ -115,6 +125,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post"> 
             <input type = "text" name = "username" placeholder = "Enter a username" <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
             <span class="invalid-feedback"><?php echo $username_err; ?></span>
+            <input type = "text" name = "userName" placeholder = "Enter a name" <?php echo (!empty($name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $name; ?>">
+            <span class="invalid-feedback"><?php echo $name_err; ?></span>
             <input type = "text" name = "email" placeholder = "Enter an email"  <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $email; ?>">
             <span class="invalid-feedback"><?php echo $email_err; ?></span>
             <input type = "password" name = "password" placeholder = "Enter a password" <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
