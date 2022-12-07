3bbf1ba80085760b631e1cc51a5c857546fc52c9
ruikeyuan1 <ruike.yuan@student.nhlstenden.com>
2022-12-07 16:06:35 +0100

    fixed issue with stylesheet linking

--- pages/userContract.php
+++ pages/userContract.php
@@ -7,7 +7,6 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<!--    <link rel = "stylesheet" href="style/stylesheet.css" type="text/css">-->
     <link rel = "stylesheet" href="Stylesheet.css" type="text/css">
     <title>Admin Panel</title>
 </head>
