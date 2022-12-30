3e4557b0f6d484826717d559c84d9469495a45d4
liafernando <ameli.masewge.fernando@student.nhlstenden.com>
2022-12-31 00:40:35 +0100

    homepage fix with links

--- pages/home.php
+++ pages/home.php
@@ -4,18 +4,18 @@
   <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="stylesheet.css">
     <title>Homepage</title>
 </head>
 
 <body>
 
 <div class="navbar">
-  <a class="home" href="home">Home</a>
-        <a href="News Service">News Service</a>
-        <a href="Ticket">Ticket</a>
-        <a href="Profile">Profile</a>
-        <a href="contactform">ContactUs</a>
+  <a class="home.php" href="#home">Home</a>
+  <a href="News Service">News Service</a>
+  <a href="Ticket.php">Ticket</a>
+  <a href="userProfile.php">Profile</a>
+  <a href="Contacttform.php">ContactUs</a>
 </div>  
 
 <div class="header">
@@ -27,21 +27,21 @@
 </div>
 
 <div class="flex-container">
-  
-      <figure class="item1">
-        <figcaption href="" class="caption">Laptop Repair</figcaption>
-        <img src="laptop.png" alt="laptop">
-      </figure>
+                
+                <figure class="item1">
+                  <figcaption href="" class="caption">Laptop Repair</figcaption>
+                  <img src="laptop.png" alt="laptop">
+                </figure>
 
-      <figure class="item2">
-        <figcaption href="" class="caption">Phone Repair</figcaption>
-        <img src="phone2.png" alt="bill">
-      </figure>
+                <figure class="item2">
+                  <figcaption href="" class="caption">Phone Repair</figcaption>
+                  <img src="phone2.png" alt="bill">
+                </figure>
 
-      <figure class="item3">
-        <figcaption href="" class="caption">Hardware/Software</figcaption>
-        <img src="hardware.png" alt="Hardware">
-      </figure>
+                <figure class="item3">
+                  <figcaption href="" class="caption">Hardware/Software</figcaption>
+                  <img src="hardware.png" alt="Hardware">
+                </figure>
 </div>
 
 </body>
