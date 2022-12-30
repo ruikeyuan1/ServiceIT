bfcec8e52bfa926bd2f0cb0c1b12fc0fde965625
liafernando <ameli.masewge.fernando@student.nhlstenden.com>
2022-12-30 19:35:30 +0100

    Database fix with description issue

--- pages/ticket.php
+++ pages/ticket.php
@@ -2,6 +2,9 @@
 session_start();
 require_once "connect.php";
 
+$user_id = $_SESSION["userId"];
+$user_name = $_SESSION["username"];
+$user_email = $_SESSION["userEmail"];
 $description = "";
 $description_err = "";
 
@@ -17,12 +20,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     
     if(empty($description_err)){
         
-        $sql = "INSERT INTO service_ticket (`description`) VALUES (?, ?, ?)";
+        $sql = "INSERT INTO service_ticket (`user_id`, `user_name`,`user_email`,`description`) VALUES (?,?,?)";
 
         if($stmt = $conn->prepare($sql)){
 
-            if($stmt->bind_param("sss",$param_description)) {
-            
+            if($stmt->bind_param("isss",$user_id,$user_name,$user_email,$param_description)) {
+
                 $param_description = $description;
                 
                 if($stmt->execute()){
@@ -52,7 +55,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
   <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="stylesheet.css">
     <title>TicketPage</title>
 </head>
 
