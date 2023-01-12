757f741bb965d36c1dcd8422943b996ae3869959
liafernando <ameli.masewge.fernando@student.nhlstenden.com>
2023-01-12 12:29:29 +0100

    The ticket page because it was deleted by mistake.

--- pages/ticket.php
+++ pages/ticket.php
@@ -1,14 +1,73 @@
 <?php
 session_start();
-require_once "connect.php";
+
 
 $user_id = $_SESSION["userId"];
 $user_name = $_SESSION["username"];
+$admin_id = 0;
+$status ="InProgress";
 $description = "";
 $description_err = "";
+$service_type = "phone_repair";
+//getSelectOptions();
 
+// function getSelectOptions() : array {
+//     require "connect.php";
+
+$array = [];
+
+
+require "connect.php";
+$sql = "SELECT `service_type` FROM `service_request` WHERE `user_id` = ? ";
+
+if($stmt = $conn->prepare($sql)){
+
+    if($stmt->bind_param("i",$user_id)) {                      
+        if($stmt->execute()){
+            echo "Your form is submitted!";
+        } else{
+            //echo "Something went wrong. Please try again later.";
+            echo "Error executing:" . $conn->error;
+        }
+
+        $stmt->store_result();
+        //print_r($stmt);
+
+        if($stmt->num_rows > 0) {
+            
+            $stmt -> bind_result($serviceType);
+            while($stmt ->fetch()) {
+                if(!in_array($serviceType,$array))
+                {
+                    array_push($array,$serviceType);
+                }
+            }
+
+                //print($serviceType);
+                // $array += [$serviceType];
+               
+        }
+
+            $stmt->close();
+    } else{
+     
+        echo "Error binding:" . $conn->error;
+    }
+} else{
+    echo "Error peparing:" . $conn->error;
+}
+
+$conn->close();
+
+//print_r($array);
+//return $array;
+
+//     }
+// }
+require "connect.php";
 if($_SERVER["REQUEST_METHOD"] == "POST"){    
 
+    $service_type = $_POST["options"];
     //Validate description
     if(empty(trim($_POST["description"]))){
         $description_err = "Please enter a description.";   
@@ -16,23 +75,17 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         $description = trim($_POST["description"]);
     }
 
+    $param_description = $description;
     
     if(empty($description_err)){
         
-        $sql = "INSERT INTO service_ticket (`user_id`, `user_name`,`description`) VALUES (?,?,?)";
+        $sql = "INSERT INTO service_ticket (`user_id`,`admin_id`,`status`,`description`,`service_type`) VALUES (?,?,?,?,?)";
 
         if($stmt = $conn->prepare($sql)){
 
-            if($stmt->bind_param("iss",$user_id,$user_name,$param_description)) {
-
-                $param_description = $description;
+            if($stmt->bind_param("iisss",$user_id,$admin_id,$status,$param_description, $service_type)) {            
                 
                 if($stmt->execute()){
-                    $get_email = mysqli_query($conn, "SELECT email FROM `user` WHERE `id` = ".$_SESSION['userId']."");
-                    $get_email = $result->fetch_assoc()['email'];
-
-                    mail($get_email, "Service IT - Thanks for the ticket!", "Thank you for sending the ticket!"); 
-
                     echo "Your form is submitted!";
                 } else{
                     //echo "Something went wrong. Please try again later.";
@@ -52,6 +105,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     
     $conn->close();
 }
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -70,14 +124,31 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <a href="Ticket">Ticket</a>
         <a href="Profile">Profile</a>
         <a href="contactform">ContactUs</a>
-    </div>  
+    </div> 
+     
 <div class="header">
     <div class="logo">SERVICE IT</div>
 </div>
+
+
     <div class="container">
    
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post"> 
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" name="selectEx" method="post"> 
         <h3>Order a ticket</h3>
+         <select name="options" id="options">
+        <?php
+
+        $serviceTypeToBeChecked="";
+            foreach ($array as $serviceType) {
+                if ($serviceTypeToBeChecked == $serviceType) {
+                    echo "<option value=$serviceTypeToBeChecked selected>$serviceTypeToBeChecked</option>";
+                }
+                else {
+                    echo "<option value=$serviceType>$serviceType</option>";
+                }
+            }
+        ?>    
+         </select>
             <label for="subject">Describe the problem</label>
             <textarea id="subject" name="description" placeholder=" " style="height:200px"></textarea>
             <input type="submit" value="Submit"> 
